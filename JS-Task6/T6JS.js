e130cd8dc528f7c991750c15a99f87559209b5c5
RAED MOUSA <raed.sam119@gmail.com>
2022-06-25 19:41:08 +0300

    Task 6 JS

---
+++ JS-Task6/T6JS.js
@@ -0,0 +1,16 @@
+// TASK 6
+
+let a = 20;
+let b = 30;
+let c = 10;
+// condition ? true : false
+
+// console.log(a ? b && a ? c || a ? b); true
+console.log((a < b && a > c) || a <= b);
+
+// console.log(a ? b ? a ? c); true
+console.log((((a < b) > a) < c));
+
+// console.log(?(a ? b) && !(a ? b) && ?(a ? c) && ? (a ? c)); true
+console.log((((a < b) && !(a > b)) && (a > c)) && (a > c));
+
