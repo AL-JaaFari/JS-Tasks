e835a469b9aba8ed0537041375590d8fbd59ef51
RAED MOUSA <raed.sam119@gmail.com>
2022-06-25 19:39:45 +0300

    Task 5 JS

---
+++ JS-Task5/T5JS.js
@@ -0,0 +1,49 @@
+
+// TASK 5
+// 1)
+console.log(150000 - 50000);
+
+// 2)
+console.log(100000);
+
+// 3)
+console.log(Math.max(100000, 25000, 70000));
+
+// 4)
+console.log(Math.min(100000, 300000, 600000));
+
+// 5)
+console.log(Math.pow(10, 5));
+
+// 6)
+console.log(Math.floor(100000.3));
+
+// 7)
+console.log(Math.round(100000.1));
+
+// 8)
+console.log(`${100000}`);
+
+// 9)
+console.log(50000 + 50000);
+
+// 10)
+console.log(100000.111.toFixed(0));
+
+// 11)
+console.log(10 ** 5);
+
+// 12)
+console.log(Number("100000"));
+
+// 13)
+console.log(1e5);
+
+// 14)
+console.log(parseInt("100000RAED"));
+
+
+
+
+
+
