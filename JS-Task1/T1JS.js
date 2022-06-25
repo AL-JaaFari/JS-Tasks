8d7a614d7f63d51285896c78e43723ba84f44356
RAED MOUSA <raed.sam119@gmail.com>
2022-06-25 19:22:26 +0300

    Task 1 JS

---
+++ JS-Task1/T1JS.js
@@ -0,0 +1,4 @@
+console.log("Hello %c JS %c Project", 
+                `color:blue; font-size:60px; font-weight:bold;`,
+                `color:red; font-size:40px; font-weight:bold; 
+                background-color:blue; padding:5px; margin: 10px;`)
